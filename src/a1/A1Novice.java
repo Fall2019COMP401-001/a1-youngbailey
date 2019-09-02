5b18d9cb7afd7facaa5e8dffdb66537fd19a68e4
bay512 <bay512@bay512-pc>
2019-09-02 00:24:55 -0400

    Completed first output in Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,44 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numOfCustomers = scan.nextInt();
 		
-	}
+		Double total = 0.0;
+		
+		String firstName = scan.next();
+		
+		String lastName = scan.next();
+		
+		int items = scan.nextInt();
+		
+		int[] amount = new int[items];
+		
+		Double[] price = new Double[items];
+		
+		String[] itemName = new String[items];
+		
+		
+	
+		 for (int i = 0; i < items; i++) {
+			amount[i] = scan.nextInt();
+			itemName[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		 
+		 
+		
+		for(int i = 0; i < amount.length; i ++) {
+			total = (Double) (total + (amount[i] * price[i]));
+		}
+		
+		
+		System.out.println(firstName.charAt(0) + "." + " " + (lastName));
+		System.out.println(total);
+				
+		scan.close();
+		
+	
 }
+
+}
+ 
